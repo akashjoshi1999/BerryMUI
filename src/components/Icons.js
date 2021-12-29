98c67b8b2bbcbb3e5dcab33454854c1260ed65b1
aakashjoshi1998 <akashjoshi61099@gmail.com>
2021-12-29 11:39:46 +0530

    intial commit

---
+++ src/components/Icons.js
@@ -0,0 +1,30 @@
+import Analytics from "./assets/Analytics";
+import Default from "./assets/Default";
+import Statatics from "./assets/Statatics";
+import Data from "./assets/Data";
+import Chart from "./assets/Chart";
+
+export const Dashboard = [
+  {
+    icon: <Default />,
+    title: "Default",
+  },
+  {
+    icon: <Analytics />,
+    title: "Analytics",
+  },
+];
+export const Widget = [
+  {
+    icon: <Statatics />,
+    title: "Statatics",
+  },
+  {
+    icon: <Data />,
+    title: "Data",
+  },
+  {
+    icon: <Chart />,
+    title: "Chart",
+  },
+];
---
+++ src/components/assets/Default.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Default = () => {
+  return (
+    <svg
+      class="icon icon-tabler icon-tabler-dashboard"
+      width="1.3rem"
+      height="1.3rem"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <circle cx="12" cy="13" r="2"></circle>
+      <line x1="13.45" y1="11.55" x2="15.5" y2="9.5"></line>
+      <path d="M6.4 20a9 9 0 1 1 11.2 0z"></path>
+    </svg>
+  );
+};
+export default Default;
